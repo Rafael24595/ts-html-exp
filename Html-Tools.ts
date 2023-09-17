a8b2b7eb81685001a814553a02111bc1f9d73595
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2023-09-17 12:07:00 +0200

    [FEATURE] Input-Clean tag created.

--- Html-Tools.ts
+++ Html-Tools.ts
@@ -2,6 +2,7 @@ class HtmlTools {
 
     public static init(): void {
         HtmlTools.buildSelectExpanded();
+        HtmlTools.buildInputClean();
     }
 
     private static buildSelectExpanded(): void {
@@ -9,6 +10,14 @@ class HtmlTools {
         for (let index = 0; index < selects.length; index++) {
             const select = selects[index] as HTMLSelectElement;
             new SelectExpanded(select);
+        }
+    }
+
+    private static buildInputClean(): void {
+        const inputs = document.getElementsByTagName('input-clean');
+        for (let index = 0; index < inputs.length; index++) {
+            const input = inputs[index] as HTMLInputElement;
+            new InputClean(input);
         }
     }
     
@@ -38,6 +47,15 @@ class SelectExpanded {
         this.selectContainer.append(this.optionsContainer);
     }
     
+    private implementStyles() {
+        if(document.getElementById("select-expanded") == undefined) {
+            const style = document.createElement('style');
+            style.id = "select-expanded";
+            style.innerHTML = SelectExpandedCss;
+            document.getElementsByTagName('head')[0].appendChild(style);
+        }
+    }
+
     private implementBaseEvents() {
         document.addEventListener('click', this.clickOutside.bind(this));
     }
@@ -45,15 +63,6 @@ class SelectExpanded {
     private clickOutside(event: MouseEvent) {
         if (this.isExpanded() && this.select !== event.target && !this.select.contains(event.target as Node)) {    
             this.expandSelect();
-        }
-    }
-
-    private implementStyles() {
-        if(document.getElementById("select-expanded") == undefined) {
-            const style = document.createElement('style');
-            style.id = "select-expanded";
-            style.innerHTML = SelectExpandedCss;
-            document.getElementsByTagName('head')[0].appendChild(style);
         }
     }
 
@@ -190,6 +199,7 @@ const SelectExpandedCss: string = `
 
     select-expanded {
         display: inline-block;
+        border: 1px solid black;
     }
 
     .select-container {
@@ -200,7 +210,10 @@ const SelectExpandedCss: string = `
 
     .select-actions-container {
         display: flex;
-        border: 1px solid black;
+    }
+
+    .select-actions-container:focus-within {
+        outline: auto;
     }
 
     .select-actions-container input {
@@ -252,6 +265,130 @@ const SelectExpandedCss: string = `
     .hidden-keep-width {
         visibility: hidden;
         height: 0px;
+    }
+
+`;
+
+class InputClean {
+
+    private inputClean: HTMLInputElement;
+    private inputContainer: HTMLDivElement;
+
+    public constructor(inputClean: HTMLInputElement) {
+        this.implementStyles();
+
+        this.inputClean = inputClean;
+        this.inputContainer = this.createInputContainer();
+
+        this.buildInputContainer();
+
+        inputClean.append(this.inputContainer);
+    }
+
+    private implementStyles() {
+        if(document.getElementById("input-clean") == undefined) {
+            const style = document.createElement('style');
+            style.id = "input-clean";
+            style.innerHTML = InputCleanCss;
+            document.getElementsByTagName('head')[0].appendChild(style);
+        }
+    }
+
+    private createInputContainer() {
+        const inputContainer = document.createElement("div");
+        inputContainer.classList.add("input-container");
+        return inputContainer;
+    }
+
+    private buildInputContainer() {
+        this.buildInput();
+        this.buildButton();
+    }
+
+    private buildInput() {
+        const value = this.inputClean.getAttribute("value");
+        const name = this.inputClean.getAttribute("name");
+        const type = this.inputClean.getAttribute("type");
+
+        const input = document.createElement("input");
+
+        if(value != undefined)
+            input.value = value;
+
+        if(name != undefined)
+            input.setAttribute("name", name);
+
+        if(type != undefined)
+            input.setAttribute("type", type);
+
+        this.inputContainer.append(input);
+    }
+
+    private buildButton() {
+        const button = document.createElement("button");
+
+        button.innerHTML = "✖";
+
+        button.onclick = this.cleanInput.bind(this);
+
+        this.inputContainer.append(button);
+    }
+
+    private cleanInput() {
+        const input = this.getInput();
+        if(input != undefined) {
+            switch (this.getInputType()) {
+                case "number":
+                    input.value = "0";
+                break;
+                default:
+                    input.value = "";
+                break;
+            }
+        }
+    }
+
+    private getInput() {
+        return this.inputContainer.getElementsByTagName("input")[0];
+    }
+
+    private getInputType() {
+        const input = this.getInput();
+        if(input != undefined) {
+            return input.getAttribute("type");
+        }
+        return "";
+    }
+
+}
+
+const InputCleanCss: string = `
+
+    input-clean {
+        display: inline-block;
+        border: 1px solid;
+    }
+
+    input-clean:focus-within {
+        outline: auto;
+    }
+
+    input-clean input {
+        border: 0px;
+        flex: 1 1 auto;
+    }
+
+    input-clean input:focus {
+        outline: none;
+    }
+
+    input-clean button {
+        border: 0px;
+        border-left: 1px solid;
+    }
+
+    .input-container {
+        display: flex;
     }
 
 `;
